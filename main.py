7d3511ac37a315a8af232f29c74c3a8355e4343d
GiaMinh0802 <93763817+GiaMinh0802@users.noreply.github.com>
2023-11-13 21:17:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ api_id = '28222709'
 api_hash = 'cbe0badef0a05463b386cac35dbfb4e9'
 client = TelegramClient('anon', api_id, api_hash)
 
-@client.on(events.NewMessage(chats=['doclenh92lottery', 'Sukien92Lottery', 'BOTWINGO92', 'doclenhvn168', 'sukien168vn', 'vngolenh3phut', 'sukienvessovn', 'VESOVN5PHUT', -1001749468867, -1001634428201]))
+@client.on(events.NewMessage(chats=['doclenh92lottery', 'Sukien92Lottery', 'BOTWINGO92', 'doclenhvn168', 'sukien168vn', 'hinhanhdangbaivn168', 'vngolenh3phut', 'lenh3p82vnk3', 'sukienvessovn', 'VESOVN5PHUT', -1001749468867, -1001634428201]))
 async def my_event_handler(event):
     msg = str(event.raw_text)
     text = msg.split("\n")
@@ -27,7 +27,7 @@ async def my_event_handler(event):
                 await client.send_message('claimgame0802', "66club: " + mess)
             else:
                 await client.send_message('claimgame0802', sender_name + ": " + mess)
-            if (sender_name == 'doclenhvn168' or sender_name == 'sukien168vn'):
+            if (sender_name == 'doclenhvn168' or sender_name == 'sukien168vn' or sender_name == 'hinhanhdangbaivn168'):
                 print('----------VN168----------')
                 vn168Redpage(mess)
             if (sender_name == 'doclenh92lottery' or sender_name == 'Sukien92Lottery' or sender_name == 'BOTWINGO92'):
@@ -36,7 +36,7 @@ async def my_event_handler(event):
             if (sender_name == 'sukienvessovn' or sender_name == 'VESOVN5PHUT'):
                 print('----------VESOVN----------')
                 vesovnRedpage(mess)
-            if (sender_name == 'vngolenh3phut'):
+            if (sender_name == 'vngolenh3phut' or sender_name == 'lenh3p82vnk3'):
                 print('----------82VN----------')
                 vn82Redpage(mess)
 
