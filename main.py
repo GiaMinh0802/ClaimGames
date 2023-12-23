c22b7339c24d7f553482518343017a4689f5925c
GiaMinh0802 <93763817+GiaMinh0802@users.noreply.github.com>
2023-12-23 05:31:14 +0000

    Fix main.py

--- main.py
+++ main.py
@@ -5,43 +5,47 @@ from vesovn.Redpage import vesovnRedpage
 from vn168.Redpage import vn168Redpage
 from club66.Redpage import clubRedpage
 
-
 print('Started')
 
 api_id = '28222709'
 api_hash = 'cbe0badef0a05463b386cac35dbfb4e9'
 client = TelegramClient('anon', api_id, api_hash)
 
-@client.on(events.NewMessage(chats=['doclenh92lottery', 'Sukien92Lottery', 'BOTWINGO92', 'doclenhvn168', 'sukien168vn', 'hinhanhdangbaivn168', 'vngolenh3phut', 'lenh3p82vnk3', 'sukienvessovn', 'VESOVN5PHUT', -1001749468867, -1001634428201]))
+async def get_chat_name(chat_id):
+    entity = await client.get_entity(chat_id)
+    return entity.title
+
+@client.on(events.NewMessage(chats=[-1001172657538, -1001842592179, -1001886568136, -1001887175434, -1001958370435, -1001966552444, -1001975632908, -1001925286709, -1001935258958, -1001808293140, -1001761532313, -1001822029296, -1002116803365, -1001634428201, -1001749468867]))
 async def my_event_handler(event):
     msg = str(event.raw_text)
     text = msg.split("\n")
-    try:
-        sender = await event.get_sender()
-        sender_name = sender.username
-    except:
-        pass
+    chat_id = event.chat_id
+    chat_name = await get_chat_name(chat_id)
     for mess in text:
         mess = mess.rstrip()
         if (len(mess) == 32 or len(mess) == 25) and (" " not in mess) and ("/" not in mess):
-            if (sender_name is None):
-                print('----------66CLUB----------')
-                clubRedpage(mess)
-                await client.send_message('claimgame0802', "66club: " + mess)
-            else:
-                await client.send_message('claimgame0802', sender_name + ": " + mess)
-            if (sender_name == 'doclenhvn168' or sender_name == 'sukien168vn' or sender_name == 'hinhanhdangbaivn168'):
-                print('----------VN168----------')
-                vn168Redpage(mess)
-            if (sender_name == 'doclenh92lottery' or sender_name == 'Sukien92Lottery' or sender_name == 'BOTWINGO92'):
+            
+            if (chat_id in [-1001172657538, -1001842592179, -1001886568136]):
                 print('----------92LOTTERY----------')
                 lotteryRedpage(mess)
-            if (sender_name == 'sukienvessovn' or sender_name == 'VESOVN5PHUT'):
+            
+            if (chat_id in [-1001887175434, -1001958370435, -1001966552444]):
+                print('----------VN168----------')
+                vn168Redpage(mess)
+                
+            if (chat_id in [-1001975632908, -1001925286709]):
                 print('----------VESOVN----------')
                 vesovnRedpage(mess)
-            if (sender_name == 'vngolenh3phut' or sender_name == 'lenh3p82vnk3'):
+
+            if (chat_id in [-1001935258958, -1001808293140, -1001761532313, -1001822029296, -1002116803365]):
                 print('----------82VN----------')
                 vn82Redpage(mess)
+
+            if (chat_id in [-1001634428201, -1001749468867]):
+                print('----------66CLUB----------')
+                clubRedpage(mess)
+
+            await client.send_message('claimgame0802', chat_name + ": " + mess)
 
 client.start()
 client.run_until_disconnected()
