f62823aee111c0f687027ae2fa48709a2bde9530
GiaMinh0802 <93763817+GiaMinh0802@users.noreply.github.com>
2023-12-25 19:33:07 +0700

    Update Redpage.py

--- vesovn/Redpage.py
+++ vesovn/Redpage.py
@@ -125,30 +125,30 @@ def vesovnRedpage(giftcode):
     signature_values = re.findall(r'"signature":"(.*?)"', auth)
 
     for phone, rand, sign, number in zip(phones, random_values, signature_values, data):
-        listBan = [8,9,10,19,30,34,43,55,65,69,82,86,96,127,143,161,183]
-        if (int(number) not in listBan):
-            token = data[number]['token']
-            try:
-                decoded_token = jwt.decode(token, verify=False)
-                expiration_time = datetime.fromtimestamp(int(decoded_token['exp']))
-                current_time = datetime.now()
-                if current_time > expiration_time:
-                    try:
-                        token = GetToken(phone.strip(), data[number]['login']['random'], data[number]['login']['sign'])
-                        data[number]['token'] = token
-    
-                        formatted_json = json.dumps(data, indent=4, sort_keys=False)
-                        with open(json_path, 'w') as file:
-                            file.write(formatted_json)
-                    except Exception as e:
-                        print(number + ":" + str(e))
-                        continue
-            except Exception as e:
-                print(e)
-            response = GetRedpage(giftcode, rand, sign, token)
-            print(number + ":" + response['msg'])
-            if response['msg'] != 'Exchange successful':
-                break
+        # listBan = [8,9,10,19,30,34,43,55,65,69,82,86,96,127,143,161,183]
+        # if (int(number) not in listBan):
+        token = data[number]['token']
+        try:
+            decoded_token = jwt.decode(token, verify=False)
+            expiration_time = datetime.fromtimestamp(int(decoded_token['exp']))
+            current_time = datetime.now()
+            if current_time > expiration_time:
+                try:
+                    token = GetToken(phone.strip(), data[number]['login']['random'], data[number]['login']['sign'])
+                    data[number]['token'] = token
+
+                    formatted_json = json.dumps(data, indent=4, sort_keys=False)
+                    with open(json_path, 'w') as file:
+                        file.write(formatted_json)
+                except Exception as e:
+                    print(number + ":" + str(e))
+                    continue
+        except Exception as e:
+            print(e)
+        response = GetRedpage(giftcode, rand, sign, token)
+        print(number + ":" + response['msg'])
+        if response['msg'] != 'Exchange successful':
+            break
 
 def main():
     print("------VESOVN------")
@@ -174,30 +174,30 @@ def main():
     signature_values = re.findall(r'"signature":"(.*?)"', auth)
 
     for phone, rand, sign, number in zip(phones, random_values, signature_values, data):
-        listBan = [8,9,10,19,30,34,43,55,65,69,82,86,96,127,143,161,183]
-        if (int(number) not in listBan):
-            token = data[number]['token']
-            try:
-                decoded_token = jwt.decode(token, verify=False)
-                expiration_time = datetime.fromtimestamp(int(decoded_token['exp']))
-                current_time = datetime.now()
-                if current_time > expiration_time:
-                    try:
-                        token = GetToken(phone.strip(), data[number]['login']['random'], data[number]['login']['sign'])
-                        data[number]['token'] = token
-    
-                        formatted_json = json.dumps(data, indent=4, sort_keys=False)
-                        with open(json_path, 'w') as file:
-                            file.write(formatted_json)
-                    except Exception as e:
-                        print(number + ":" + str(e))
-                        continue
-            except Exception as e:
-                print(e)
-            response = GetRedpage(giftcode, rand, sign, token)
-            print(number + ":" + response['msg'])
-            if response['msg'] != 'Exchange successful':
-                break
+        # listBan = [8,9,10,19,30,34,43,55,65,69,82,86,96,127,143,161,183]
+        # if (int(number) not in listBan):
+        token = data[number]['token']
+        try:
+            decoded_token = jwt.decode(token, verify=False)
+            expiration_time = datetime.fromtimestamp(int(decoded_token['exp']))
+            current_time = datetime.now()
+            if current_time > expiration_time:
+                try:
+                    token = GetToken(phone.strip(), data[number]['login']['random'], data[number]['login']['sign'])
+                    data[number]['token'] = token
+
+                    formatted_json = json.dumps(data, indent=4, sort_keys=False)
+                    with open(json_path, 'w') as file:
+                        file.write(formatted_json)
+                except Exception as e:
+                    print(number + ":" + str(e))
+                    continue
+        except Exception as e:
+            print(e)
+        response = GetRedpage(giftcode, rand, sign, token)
+        print(number + ":" + response['msg'])
+        if response['msg'] != 'Exchange successful':
+            break
 
 if __name__ == "__main__":
     main()
