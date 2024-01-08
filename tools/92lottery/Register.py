0385c077463f275a3f3954bbf2a519c0ddb9231b
GiaMinh <vogiaminh0802@gmail.com>
2024-01-08 23:41:51 +0700

    Check Account

--- tools/92lottery/Register.py
+++ tools/92lottery/Register.py
@@ -1,23 +1,51 @@
 import requests
 import sys
+import random
+import win32com.shell.shell as shell
+import time
 
-phone = "+84" + sys.argv[1]
+def ResetDcom(nameDcom):
+    commands = 'netsh interface set interface "' + nameDcom + '" disable'
+    shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands)
+    time.sleep(1)
+    commands = 'netsh interface set interface "' + nameDcom + '" enable'
+    shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands)
+    ip = ""
+    i = 0
+    while ip == "":
+        if (i == 5):
+            commands = 'netsh interface set interface "' + nameDcom + '" disable'
+            shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands)
+            time.sleep(1)
+            commands = 'netsh interface set interface "' + nameDcom + '" enable'
+            shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands)
+            i = 0
+        try:
+            ip = requests.get('https://api.ipify.org/?format=json').json()["ip"]
+            i = i + 1
+        except:
+            pass
+
+listInvite = ['Qtckx364592', 'Mcv7n462062','996427','21QCq5958','4PPxk189497','rb89R284037','UaUdr352255']
+inviteCode = random.choice(listInvite)
+phone = sys.argv[1]
 
 param = {
     "username": phone,
     "smsvcode": "",
     "pwd": "GiaMinh123",
     "regtype": "",
-    "invitecode": "Qs35D611400",
+    "invitecode": inviteCode,
     "domainurl": "92lottery.com",
     "phonetype": "0",
     "language": "vi"
 }
+ResetDcom("Cellular 7")
 
 response = requests.post('https://92lotteryapi.com/api/webapi/Register', data=param)
 response = response.json()
+
 if (response['success'] is True):
-    print(response['data']['UserId'])
-    print(response['data']['Sign'])
+    print(response['data']['UserId']) 
 else:
     print(response)
