5d4f82f58f22f6d688d57ea0559d1a00a7bedd91
GiaMinh0802 <vogiaminh0802@gmail.com>
2024-01-11 20:21:49 +0700

    Register lottery

--- tools/92lottery/Register.py
+++ tools/92lottery/Register.py
@@ -1,30 +1,6 @@
 import requests
 import sys
 import random
-import requests
-import win32com.shell.shell as shell
-import time
-
-def ResetDcom(nameDcom):
-    max_attempts = 5
-    attempts = 0
-    while attempts < max_attempts:
-        commands_disable = f'netsh interface set interface "{nameDcom}" disable'
-        shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands_disable)
-        time.sleep(1)
-        commands_enable = f'netsh interface set interface "{nameDcom}" enable'
-        shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c ' + commands_enable)
-        ip = ""
-        while ip == "":
-            try:
-                ip = requests.get('https://api.ipify.org/?format=json').json()["ip"]
-            except:
-                pass
-        if ip != "":
-            break
-        attempts += 1
-    if attempts == max_attempts:
-        ResetDcom(nameDcom)
 
 listInvite = ['Qtckx364592', 'Mcv7n462062','996427','21QCq5958','4PPxk189497','rb89R284037','UaUdr352255']
 inviteCode = random.choice(listInvite)
@@ -40,8 +16,6 @@ param = {
     "phonetype": "0",
     "language": "vi"
 }
-
-ResetDcom("Mobile 2")
 
 response = requests.post('https://92lotteryapi.com/api/webapi/Register', data=param)
 response = response.json()
