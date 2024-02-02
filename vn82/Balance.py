25933dd731aade677e12e74b22c7d3bc53cd3def
GiaMinh0802 <vogiaminh0802@gmail.com>
2024-02-02 10:29:07 +0700

    Update Balance

--- vn82/Balance.py
+++ vn82/Balance.py
@@ -6,15 +6,15 @@ json_path = "vn82/key.json"
 
 def GetBanlance(random, sign, token):
     # Yêu cầu OPTIONS
-    options_url = "https://82vn82vnapi/api/webapi/GetUserInfo"
+    options_url = "https://82vn82vnapi.com/api/webapi/GetUserInfo"
     options_headers = {
-        "authority": "vn168api.com",
+        "authority": "82vn82vnapi.com",
         "accept": "*/*",
         "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
         "access-control-request-headers": "authorization,content-type",
         "access-control-request-method": "POST",
-        "origin": "https://vn168.com",
-        "referer": "https://vn168.com/",
+        "origin": "https://82vn.com",
+        "referer": "https://82vn.com/",
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "cross-site",
@@ -23,15 +23,13 @@ def GetBanlance(random, sign, token):
     requests.options(options_url, headers=options_headers)
 
     # Yêu cầu POST
-    post_url = "https://82vn82vnapi/api/webapi/GetUserInfo"
+    post_url = "https://82vn82vnapi.com/api/webapi/GetUserInfo"
     post_headers = {
-        "authority": "vn168api.com",
         "accept": "application/json, text/plain, */*",
-        "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
         "authorization": "Bearer " + token,
         "content-type": "application/problem+json; charset=UTF-8",
-        "origin": "https://vn168.com",
-        "referer": "https://vn168.com/",
+        "origin": "https://82vn.com",
+        "referer": "https://82vn.com/",
         "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": "\"Windows\"",
